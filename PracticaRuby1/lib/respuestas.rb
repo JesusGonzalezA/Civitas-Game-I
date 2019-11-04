ed3142c773d163153d4a09089f644b3e247d1cd0
Jesús González Álvarez <jesusgranada99@gmail.com>
2019-11-04 16:13:36 +0100

    Añadido nuevo enumerado

---
+++ PracticaRuby1/lib/respuestas.rb
@@ -0,0 +1,9 @@
+module Civitas
+  module Respuestas
+      SI =:si
+      NO =:no
+  end
+end
+
+## USO LISTAS RESPUESTAS COMO VARIABLE GLOBAL
+#$lista_Respuestas = [Civitas::Respuestas::SI, CIVITAS::RESPUESTAS::NO]
