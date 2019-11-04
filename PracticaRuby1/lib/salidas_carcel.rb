3d2a2ced0b4a42ac25e4780b0fe16d93f646ae07
Jesús González Álvarez <jesusgranada99@gmail.com>
2019-11-04 12:12:06 +0100

    Añado nuevos tipos enumerados

---
+++ PracticaRuby1/lib/salidas_carcel.rb
@@ -0,0 +1,14 @@
+module Civitas
+  module Salidas_carcel
+      
+    PAGANDO     =:pagando
+    TIRANDO     =:tirando
+    
+    lista_Salidas_carcel = [
+                            Salidas_carcel::PAGANDO,
+                            Salidas_carcel::TIRANDO
+                            ]
+                           
+    
+  end
+end
