68876cf7e7e61691b51b56a11171cf819cdf4c52
Jesús González Álvarez <jesusgranada99@gmail.com>
2019-11-05 13:18:48 +0100

    Añado el programa principal

---
+++ PracticaRuby1/lib/programa_principal.rb
@@ -0,0 +1,29 @@
+#encoding:utf-8
+
+require_relative 'civitas_juego'
+require_relative 'vista_textual'
+require_relative 'controlador'
+
+module Civitas
+class ProgramaPrincipal
+  def self .main
+    
+    #Creo jugadores
+    nombres_jugadores = []
+    nombres_jugadores << "Jesus"
+    nombres_jugadores << "Nuria"
+    
+    #Creo juego en modo debug
+    juego = CivitasJuego.new(nombres_jugadores)
+    Dado.instance.set_debug(true)
+    
+    #Jugar
+    interfaz = Vista_textual.new
+    controlador = Controlador.new(juego, interfaz)
+    controlador.juega
+  end
+end
+
+ProgramaPrincipal.main
+
+end
